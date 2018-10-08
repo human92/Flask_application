97506eb36582dbf0ed983f30284543beee28d81f
kai <kth6rtuo8@gmail.com>
2018-10-08 14:44:45 +0900

    on-going status of mysql⇒json

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,5 +1,6 @@
 from flask import request, redirect, url_for, render_template, flash
 from flaskr import app, db, ma
+from flask import Response
 from flask import Flask, jsonify, current_app
 from flaskr.models import AirPassenger, AirPassengerScheme
 # import json
@@ -11,5 +12,6 @@ def show_data():
     airpassenger=AirPassenger.query.all()
     with app.app_context():
         result=air_shema.jsonify(airpassenger)
-    return render_template('show_data.html', result=result)
-
+        resp=Response(result, status=200, mimetype='application/json')
+    return resp
+    # return render_template('show_data.html', result=result)
